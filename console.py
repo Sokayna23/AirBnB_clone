ffee844efee7058394156c17a847994f864b2c49
Sokayna <sokayna.bennasseur@gmail.com>
2023-08-09 13:34:12 -0400

    Task6: the console

--- console.py
+++ console.py
@@ -1,1 +1,23 @@
-#!/usr/bin/env bash
+#!/usr/bin/python3
+"""Console module"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """HBNBCommand class"""
+    prompt = '(hbnb) '
+    file = None
+
+    def do_quit(self, line):
+        """Exit the program"""
+        exit()
+
+    def do_EOF(self, line):
+        return True
+
+
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
