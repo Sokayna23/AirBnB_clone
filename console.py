4c0758247a6c78df41465b2a9b7a5c99d17c9961
abdoneverland <ch.abdel.news@gmail.com>
2023-08-10 10:09:25 +0100

    task 7

--- console.py
+++ console.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """Console module"""
-
 import cmd
-
+from models.base_model import BaseModel
+import models
 
 class HBNBCommand(cmd.Cmd):
     """HBNBCommand class"""
     prompt = '(hbnb) '
     file = None
+    classesnames = {"BaseModel":BaseModel}
 
     def do_quit(self, line):
         """Quit to exit the program"""
@@ -22,6 +23,64 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Disables the repetition of the last command"""
         pass
+    
+    def do_create(self, _args):
+        """Creates a new instance from class name"""
+        args = _args.split()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in HBNBCommand.classesnames:
+            print("** class doesn't exist **")
+        else:
+            print(HBNBCommand.classesnames[args[0]]().id)
+            models.storage.save()
+    def do_show(self, _args):
+        """Prints the string representation of an instance based on the class name and id"""
+        args = _args.split()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in HBNBCommand.classesnames:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif f"{args[0]}.{args[1]}" not in models.storage.all():
+            print("** no instance found **")
+        else:
+            print(models.storage.all()[f"{args[0]}.{args[1]}"])
+    def do_destroy(self, _args):
+        """Deletes an instance based on the class name and id"""
+        args = _args.split()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in HBNBCommand.classesnames:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif f"{args[0]}.{args[1]}" not in models.storage.all():
+            print("** no instance found **")
+        else:
+            models.storage.all().pop(f"{args[0]}.{args[1]}")
+            models.storage.save()
+    def do_all(self, _args):
+        """Prints all string representation of all instances based or not on the class name"""
+        args = _args.split()
+        if len(args) == 0:
+            # prints all
+            ss = []
+            for k,v in models.storage.all().items():
+                ss.append(str(v))
+            print(ss)
+        elif args[0] not in HBNBCommand.classesnames:
+            print("** class doesn't exist **")
+        else:
+            # prints specific class
+            ss = []
+            for k,v in models.storage.all().items():
+                if args[0] == v.__class__.__name__:
+                    ss.append(str(v))
+            print(ss)
+            
+            
 
 
 
--- models/base_model.py
+++ models/base_model.py
@@ -4,29 +4,29 @@ import uuid
 from datetime import datetime
 import models
 
+
 class BaseModel:
     """ class BaseModel"""
 
     def __init__(self, *args, **kwargs):
         """ init function"""
-        from models import storage
         if len(kwargs) == 0:
-            #create a new object
+            # create a new object
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             models.storage.new(self)
         else:
-            #restore from dict
+            # restore from dict
             print("restoring object from dict")
-            for key,value in kwargs.items():
+            for key, value in kwargs.items():
                 if key != "__class__":
                     self.__dict__[key] = value
-            
-            self.created_at = datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-            self.updated_at = datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
-            
-         
+
+            self.created_at = datetime.strptime(kwargs["created_at"],
+                                                "%Y-%m-%dT%H:%M:%S.%f")
+            self.updated_at = datetime.strptime(kwargs["updated_at"],
+                                                "%Y-%m-%dT%H:%M:%S.%f")
 
     def __str__(self):
         """ str function"""
@@ -43,5 +43,4 @@ class BaseModel:
         d["__class__"] = __class__.__name__
         d["created_at"] = self.created_at.isoformat()
         d["updated_at"] = self.updated_at.isoformat()
-                
         return d
