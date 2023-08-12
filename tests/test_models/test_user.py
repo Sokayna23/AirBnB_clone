e28e74f97a6a4ce8c866a979c7daa39f29aa65aa
abdoneverland <ch.abdel.news@gmail.com>
2023-08-12 12:28:25 +0100

    fixing pycodestyle

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -18,3 +18,32 @@ class User_Testing(unittest.TestCase):
         b1 = User()
         b2 = User()
         self.assertNotEqual(b1.id, b2.id)
+
+    def test_2(self):
+        """
+        testing email
+        """
+        u1 = User()
+        self.assertEqual(u1.email, "")
+
+    def test_3(self):
+        """
+        testing password
+        """
+        u1 = User()
+        self.assertEqual(u1.password, "")   
+
+    def test_4(self):
+        """
+        testing first_name
+        """
+        u1 = User()
+        self.assertEqual(u1.first_name, "")
+
+    def test_5(self):
+        """
+        testing last_name
+        """
+        u1 = User()
+        self.assertEqual(u1.last_name, "")
+
