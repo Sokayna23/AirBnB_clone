3c08ed24b08029569730adae1294494cad56ed74
abdoneverland <ch.abdel.news@gmail.com>
2023-08-12 13:19:47 +0100

    fixing pycodestyle

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -14,3 +14,10 @@ class Review_Testing(unittest.TestCase):
         b1 = Review()
         b2 = Review()
         self.assertNotEqual(b1.id, b2.id)
+        
+    def test_2(self):
+        """ testing if a unique id was generated"""
+        b1 = Review()
+        self.assertNotEqual(b1.place_id, "")
+        self.assertNotEqual(b1.user_id, "")
+        self.assertNotEqual(b1.text, "")
