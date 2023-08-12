671cc08e5ef71fd5550984fc14dfae7f7242bebb
abdoneverland <ch.abdel.news@gmail.com>
2023-08-12 13:40:23 +0100

    fixing pycodestyle

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -12,3 +12,9 @@ class BaseModel_Testing(unittest.TestCase):
         b1 = BaseModel()
         b2 = BaseModel()
         self.assertNotEqual(b1.id, b2.id)
+
+    def test_2(self):
+        """ test: creating object from another"""
+        b1 = BaseModel()
+        b2 = BaseModel(b1.to_dict())
+        self.assertEqual(b1.id, b2.id)    
