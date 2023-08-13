8f50668c574a11b6ab1e63b79de92d804ec29918
abdoneverland <ch.abdel.news@gmail.com>
2023-08-13 18:18:51 +0100

    test_base_model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -18,7 +18,7 @@ class BaseModel_Testing(unittest.TestCase):
         """ test: creating object from another"""
         b1 = BaseModel()
         d1 = b1.to_dict()
-        b2 = BaseModel(**d1)
+        b2 = BaseModel(d1)
         self.assertEqual(b1.id, b2.id)
 
     def test_3(self):
@@ -35,4 +35,3 @@ class BaseModel_Testing(unittest.TestCase):
             self.fail("BaseModel is not a class")
         except AttributeError:
             pass
-          
