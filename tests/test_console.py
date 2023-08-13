f0bbde01031284b33569752492569d8e16190347
abdoneverland <ch.abdel.news@gmail.com>
2023-08-13 09:44:01 +0100

    test_console

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """testing BaseModel"""
-from models.base_model import BaseModel
 import unittest
 import console
 
@@ -11,7 +10,11 @@ class Console_Testing(unittest.TestCase):
     """
 
     def test_1(self):
-        """ testing if a unique id was generated"""
-        b1 = BaseModel()
-        b2 = BaseModel()
-        self.assertNotEqual(b1.id, b2.id)
+        """ testing class doc"""
+        self.assertIsNotNone(console.__doc__)
+
+    def test_2(self):
+        """ testing methods doc"""
+        for m in dir(console) :
+            if not m.startswith('_'):
+                self.assertIsNotNone(getattr(console, m).__doc__)
