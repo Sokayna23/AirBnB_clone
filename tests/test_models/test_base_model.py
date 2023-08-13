9f4d5b9485e5ca253f9734c617e599d521a40bef
abdoneverland <ch.abdel.news@gmail.com>
2023-08-13 18:16:54 +0100

    test_base_model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -17,12 +17,22 @@ class BaseModel_Testing(unittest.TestCase):
     def test_2(self):
         """ test: creating object from another"""
         b1 = BaseModel()
-        b2 = BaseModel(b1.to_dict())
+        d1 = b1.to_dict()
+        b2 = BaseModel(**d1)
         self.assertEqual(b1.id, b2.id)
 
     def test_3(self):
-        """ test: creating object from another"""
+        """ test: type of attributes"""
         b1 = BaseModel()
         self.assertEqual(type(b1.id), str)
         self.assertEqual(type(b1.created_at), datetime)
         self.assertEqual(type(b1.updated_at), datetime)
+    def test_4(self):
+        """ test: type of attributes"""
+        try:
+            b1 = BaseModel
+            a = b1.id
+            self.fail("BaseModel is not a class")
+        except AttributeError:
+            pass
+          
