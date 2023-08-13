9602611f51e7a9e8185790c9259be24139bce042
abdoneverland <ch.abdel.news@gmail.com>
2023-08-13 09:57:05 +0100

    test_console

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """testing BaseModel"""
 import unittest
+from unittest.mock import patch
+from io import StringIO
+from console import HBNBCommand
 import console
-
+import uuid
 
 class Console_Testing(unittest.TestCase):
     """
@@ -18,3 +21,13 @@ class Console_Testing(unittest.TestCase):
         for m in dir(console) :
             if not m.startswith('_'):
                 self.assertIsNotNone(getattr(console, m).__doc__)
+    
+    def test_3(self):
+        """testing validty of uuid"""
+        with patch("sys.stdout", new=StringIO()) as f:
+                    HBNBCommand().onecmd("create Place")
+                    result = f.getvalue().strip()
+                    try:
+                        uuid.UUID(result)
+                    except ValueError:
+                        self.fail("not a valid uuid")
