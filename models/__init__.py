f3a66910d7b050e58668d4c450af7441d182263d
abdoneverland <ch.abdel.news@gmail.com>
2023-08-13 07:51:54 +0100

    update

--- models/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""
-Initializing package
-"""
-from models.engine.file_storage import FileStorage
-
-
-storage = FileStorage()
-storage.reload()
--- models/engine/__init__.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-"""
-Initializing package
-"""
