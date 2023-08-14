cc961710a728dc5e80cc95122ad6f58fcea33d96
abdoneverland <ch.abdel.news@gmail.com>
2023-08-14 11:41:48 +0100

    test_console

--- tests/test_console.py
+++ tests/test_console.py
@@ -21,10 +21,6 @@ class Console_Testing(unittest.TestCase):
     testing console
     """
 
-    def test_tt(self):
-        """checking an idea"""
-        self.assertEqual("(hbnb) ", HBNBCommand.prompt)
-
     def test_1(self):
         """ testing class doc"""
         self.assertIsNotNone(console.__doc__)
@@ -47,3 +43,13 @@ class Console_Testing(unittest.TestCase):
                     uuid.UUID(result)
                 except ValueError:
                     self.fail(f"not a valid uuid for {cl}")
+
+    def test_4(self):
+        """checking prompt"""
+        self.assertEqual("(hbnb) ", HBNBCommand.prompt)
+
+    def test_5(self):
+        """empty line"""
+        with patch("sys.stdout", new=StringIO()) as result:
+                    self.assertFalse(HBNBCommand().onecmd(""))
+                    self.assertEqual("", result.getvalue().strip())
